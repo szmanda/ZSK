c99f98d3d9a73c13faec5df50175fe720ea75e38
szmanda <michalszmanda.mail@gmail.com>
2019-10-01 18:52:35 +0200

    poprawienie i uzupełnienie kodu z lekcji

--- klasa4/CPP/01_10_2019_main.cpp
+++ klasa4/CPP/01_10_2019_main.cpp
@@ -10,27 +10,37 @@ int main()
 
     srand(time(NULL));
 
-    int tab[10]
+    int tab[10];
     for (int i=0; i<10; i++){
-        tab[i] = rand(%100)+1;
+        tab[i] = rand()%100+1;
     }
 
-    sortedTab[10] = tab[10];
-
-    for (int i=0; i<10: i++){
-        for (int j=i; j>0; j++){
-            while (sortedTab[i]<sortedTab[j]){
-                int pom = sortedTab[j];
-                sortedTab[j] = sortedTab[i];
-                sortedTab[i] = pom;
-            }
-        }
-        cout<<"tablica: ";
-        for (int i=0;i<10;i++){
-        cout<<sortedTab[i]<<", ";
-        }
-        cout<<endl;
+    int sortedTab[10];
+    for (int i=0; i<10; i++){
+        sortedTab[i] = tab[i];
     }
+
+    // sortowanie przez wstawianie:
+    int licznikZamian = 0;
+
+    for (int i=1; i<10; i++){ //wykonuje n-1 kroków, gdzie n to liczba sortowanych el.
+      int j=i;
+      // w każdym kroku uwzględniane jest n+1 elementow,
+      // każdy nowy jest wstawiany, na odpowiadające mu miejsce
+      while ((sortedTab[j]>sortedTab[j-1])&&(j>0)){
+        licznikZamian += 1;
+        int pom = sortedTab[j-1];
+        sortedTab[j-1] = sortedTab[j];
+        sortedTab[j] = pom;
+        j--;
+      }
+      cout<<"tab: ";
+      for (int j=0;j<10;j++){
+        cout<<sortedTab[j]<<", ";
+      }
+      cout<<endl;
+    }
+    cout<<"posortowano dokonujac "<<licznikZamian<<" zamian elementow"<<endl;
 
     return 0;
 }
