6453a2826f11f456262ab782651977c1de82df59
Michał Szmanda <32749003+szmanda@users.noreply.github.com>
2019-10-18 09:34:30 +0200

    Add files via upload

---
+++ klasa4/PIA/18_10_2019_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>formularze</title>
+  </head>
+  <body>
+    <form action="18_10_2019_1.php" method="get">
+      <input type="text" name="surname" placeholder="podaj nazwisko">
+      <input type="submit" value="wyślij">
+    </form>
+  </body>
+</html>
