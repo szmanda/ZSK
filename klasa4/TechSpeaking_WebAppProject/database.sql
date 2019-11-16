4e946dfb3baadc28ef934eb8af84336ab1bcb966
szmanda <michalszmanda.mail@gmail.com>
2019-11-16 12:04:16 +0100

    Updated encoding and auto_increment property

--- klasa4/TechSpeaking_WebAppProject/database.sql
+++ klasa4/TechSpeaking_WebAppProject/database.sql
@@ -1,15 +1,17 @@
-CREATE DATABASE Techspeak;
+CREATE DATABASE Techspeak
+  CHARACTER SET utf8
+  COLLATE utf8_general_ci;
 
 USE Techspeak;
 
 CREATE TABLE userStatus (
-  id_status INT(10) UNSIGNED NOT NULL,
+  id_status INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   status_name VARCHAR(25) NULL,
   PRIMARY KEY (id_status)
 )ENGINE=INNODB;
 
 CREATE TABLE userRank (
-  id_rank INT(10) UNSIGNED NOT NULL,
+  id_rank INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   rank_name VARCHAR(25) NULL,
   icon_path VARCHAR(100) NULL,
   PRIMARY KEY (id_rank)
@@ -18,7 +20,7 @@ CREATE TABLE userRank (
 -- needs a table UserRank containing rank names
 -- needs a table UserStatus containing statuses of the users (eg. active, deleted)
 CREATE TABLE stdUser (
-  id_user INT(10) UNSIGNED NOT NULL,
+  id_user INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   login VARCHAR(25) NOT NULL UNIQUE,
   password VARCHAR(25) NOT NULL,
   email VARCHAR(100) NOT NULL,
@@ -34,7 +36,7 @@ CREATE TABLE stdUser (
 )ENGINE=INNODB;
 
 CREATE TABLE lang(
-  id_lang INT(10) UNSIGNED NOT NULL,
+  id_lang INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   lang_name VARCHAR(25) NOT NULL,
   icon_path VARCHAR(100) NULL,
   description VARCHAR(250) NULL,
@@ -42,14 +44,14 @@ CREATE TABLE lang(
 )ENGINE=INNODB;
 
 CREATE TABLE wordCategory(
-  id_category INT(10) UNSIGNED NOT NULL,
+  id_category INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   category_name VARCHAR(25) NOT NULL,
   PRIMARY KEY (id_category)
 )ENGINE=INNODB;
 
 -- needs a table lang and table wordCategory
 CREATE TABLE word (
-  id_word INT(10) UNSIGNED NOT NULL,
+  id_word INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   word VARCHAR(50) NULL,
   definition VARCHAR(255) NULL,
   id_translation INT(10) UNSIGNED NULL,
