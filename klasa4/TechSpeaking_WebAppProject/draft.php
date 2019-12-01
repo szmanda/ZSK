5d59b42979b0cb48765e7a665509c91617fff13c
szmanda <michalszmanda.mail@gmail.com>
2019-12-01 15:54:49 +0100

    changes are about to be made

---
+++ klasa4/TechSpeaking_WebAppProject/draft.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      require_once(./values.php)
+      
+      //header
+    ?>
+  </body>
+</html>
---
+++ klasa4/TechSpeaking_WebAppProject/values.php
@@ -0,0 +1,15 @@
+<?php
+// this file contains critical values, used in the project
+
+// theme and color
+  // for string val. use dechex(NAME);
+  define('BG_COLOR_PRIMARY',0x0A4008);
+  define('BG_COLOR_SECONDARY',0x40391A);
+  define('BG_COLOR_ACCENT',0x82A633);
+  define('COLOR_PRIMARY',0xE0E8A0);
+  define('COLOR_SECONDARY',0x878C61);
+  define('COLOR_ACCENT',0x95D904);
+
+// paths
+
+?>
