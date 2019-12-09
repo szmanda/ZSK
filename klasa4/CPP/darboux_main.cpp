aaa393e4cc1faa759698de51317912b5f878dd04
Michał Szmanda <32749003+szmanda@users.noreply.github.com>
2019-12-09 18:57:51 +0100

    Update darboux_main.cpp

--- klasa4/CPP/darboux_main.cpp
+++ klasa4/CPP/darboux_main.cpp
@@ -3,25 +3,21 @@
 
 using namespace std;
 
-double fun(double x,double tab[],int len){
-    double wynik=0;
-    /*for (int i = 0; i<len; i++){
-        wynik += tab[i]*pow(x,len-i);
-    }*/
-    return 3*pow(x,11) + 2*pow(x,7) + -34*pow(x,4)+20;
-}
-
 double fun(double x){
     return 3*pow(x,11) + 2*pow(x,7) + -34*pow(x,4)+20;
 }
 
 double funRek(double x,double y){
-    if (fun(x)*fun(y)){
-        if ((y-x)<0.00001)
-            cout<<"funkcja przyjmuje wartosc 0 w przedz: <"<<x<<";"<<y<<">";
-        else
-            funRek(x,x+(x-y)/2);
-            funRek(x+(x-y)/2,y);
+    double step = (y-x)/2;
+    //cout<<"x="<<x<<" y="<<y<<endl;
+    if (fun(x)*fun(y)<=0){
+        if ((y-x)<=0.0000001){
+            cout<<"\tfunkcja przyjmuje wartosc 0 w przedz: <"<<x<<";"<<y<<">\n";
+        }
+        else{
+            funRek(x+step,y);
+            funRek(x,x+step);
+        }
     }
     return 0;
 }
@@ -30,9 +26,7 @@ int main()
 {
     cout<<"Tw darboux"<<endl;
     double start = -100, stop=100;
-    double ile=10000;
-
-    cout<<"podaj wspolczynniki dla x^n:\n";
+    double ile=1000;
 
 
     double step = (stop-start)/ile;
@@ -42,12 +36,14 @@ int main()
     int len = 3;
 
     while(cursor<stop){
-        if (fun(cursor,tab,len)*fun(cursor+step,tab,len)<=0)
-            cout<<"funkcja przeciela os OX w przedziale <"<<cursor<<";"<<cursor+step<<">\n";
+        if (fun(cursor)*fun(cursor+step)<=0){
+            cout<<"\n\nfunkcja przeciela os OX w przedziale <"<<cursor<<";"<<cursor+step<<">\n";
+            cout<<"     -- rekurencyjne zwiekszenie dokladnosci:\n";
+            funRek(cursor,cursor+step);
+        }
+
         cursor+=step;
     }
-
-    funRek(-100,100);
 
 
     return 0;
