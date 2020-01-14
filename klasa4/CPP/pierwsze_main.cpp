98f9f60fc6ab0d88949ba50d5b53c808da6b36f3
Michał Szmanda <32749003+szmanda@users.noreply.github.com>
2020-01-14 14:55:19 +0100

    Add files via upload

--- klasa4/CPP/pierwsze_main.cpp
+++ klasa4/CPP/pierwsze_main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <math.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
 bool tab[1000000];
 
+int nwd(int a, int b){
+    if (b==0) return a;
+    return nwd(b,a%b);
+}
+
 int main()
 {
+    srand(time(NULL));
+
     int sqr = 1000;
 
     for (int i=0; i<sqr*sqr;i++)
@@ -28,20 +37,48 @@ int main()
     }
     for (int i=2; i<sqr*sqr; i++){
         if (tab[i]==true){
-            cout<<i<<endl;
+            //cout<<i<<endl;
         }
     }
     int p=0,q=0;
-    for (int i=sqr*sqr; i>2; i--){
+    int los = rand()%sqr;
+    for (int i=los+sqr; i>2; i--){
         if (tab[i]==true){
             if (p!=0){
-                q=
+                q = i;
                 break;
+            } else {
+                p = i;
+                i = i-los;
             }
         }
     }
+    cout<<"p="<<p<<endl;
+    cout<<"q="<<q<<endl;
 
+    int n = (p-1)*(q-1);
+    cout<<"n="<<n<<endl;
 
+    int e;
+    // e jest bledne
+    for (int i=sqr;i>2;i--){
+        if (nwd(n,i)==1) {
+            e = i;
+            break;
+        }
+    }
+
+    cout<<"e="<<e<<endl;
+
+    int d;
+
+    for (int i=n-1;i>2;i--){
+        if ((e*i)%n==0){
+            d = i;
+            break;
+        }
+    }
+    cout<<"d="<<d<<endl;
 
 
     return 0;
