206585237eb57f2b1796ac2ccde0db056948981e
szmanda <michalszmanda.mail@gmail.com>
2020-03-20 19:33:40 +0100

    +rozw od CKE

--- klasa4/CPP/matura2017/prostokat.cpp
+++ klasa4/CPP/matura2017/prostokat.cpp
@@ -20,15 +20,38 @@ int main()
       i++;
     }
   }
-  int maxA = 0, maxB = 0;
-  for (int i=0; i<c; i++){
-    if (B[i]>B[maxA]) maxA=i;
+  if (c>1){
+    int maxA = 0, maxB = 0;
+    for (int i=0; i<c; i++){
+      if (B[i]>B[maxA]) maxA=i;
+    }
+    maxB = (maxA==0) ? 1 : 0;
+    for (int i=0; i<c && i!=maxA; i++){
+      if (B[i]>B[maxB]) maxB=i;
+    }
+    cout<< B[maxA]*B[maxB];
   }
-  maxB = (maxA==0) ? 1 : 0;
-  for (int i=0; i<c && i!=maxA; i++){
-    if (B[i]>B[maxB]) maxB=i;
+  else {
+    cout<<0;
   }
-  cout<< B[maxA]*B[maxB];
+
+  //od CKE
+  int max1,max2;
+  max1 = max2 = 0;
+  for(int i = 1; i <= n; ++i)
+  {
+    if(A[i] % p != 0)
+    {
+      if(A[i] > max1)
+      {
+        max2 = max1;
+        max1 = A[i];
+      }
+      else if(A[i] > max2)
+        max2 = A[i];
+    }
+  }
+  cout << max1 * max2;
 
   return 0;
 }
